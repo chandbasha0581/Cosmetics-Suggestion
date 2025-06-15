17152d002f0ba24e5bed583af13bfbda491fd044
chandbasha0581 <136013276+chandbasha0581@users.noreply.github.com>
2025-06-16 01:33:41 +0530

    Update next.js

--- next.js
+++ next.js
@@ -1,6 +1,17 @@
-const headers =
+/**const headers =
   process.env.VERCEL_SKEW_PROTECTION_ENABLED === '1'
     ? { 'x-deployment-id': process.env.VERCEL_DEPLOYMENT_ID }
     : {};
  
 const res = await fetch('/get', { headers });
+*/
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    useDeploymentId: true,
+    // Optionally, use with Server Actions
+    useDeploymentIdServerActions: true,
+  },
+};
+ 
+module.exports = nextConfig;
